acac3981cc7eb64f86c629ecc30d28a7cc422b92
AlenaLoik <CrazyBeeLviv@gmail.com>
2019-06-10 17:11:04 +0300

    Updates

---
+++ precache-manifest.a1afa8a634394c65f8ffb4c217d7a32f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882d8002aa0d2f097630b132c46768a6",
+    "url": "/fe_on_feb19-phones-react/index.html"
+  },
+  {
+    "revision": "1b16fc3f0bc4fcdb0034",
+    "url": "/fe_on_feb19-phones-react/static/css/main.95ac9038.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/fe_on_feb19-phones-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1b16fc3f0bc4fcdb0034",
+    "url": "/fe_on_feb19-phones-react/static/js/main.88862258.chunk.js"
+  },
+  {
+    "revision": "13ef023f66a5d3f30d63",
+    "url": "/fe_on_feb19-phones-react/static/js/runtime~main.6b2147bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fe_on_feb19-phones-react/precache-manifest.4321a3f769abe4bdae7dfdebc8fba4d9.js"
+  "/fe_on_feb19-phones-react/precache-manifest.a1afa8a634394c65f8ffb4c217d7a32f.js"
 );
 
 self.addEventListener('message', (event) => {
